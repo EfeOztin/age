0dae34a9bad5d59036400067efe1c8697c36badb
EfeÖ <efeoztin07@gmail.com>
2024-01-12 22:43:58 +0300

    2

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 function subM(submenuId, iconId) {
     var submenu = document.getElementById(submenuId);
     var icon = document.getElementById(iconId);
-    if (submenu.style.display === "block" || submenu.style.display === "") {
+    if (submenu.style.display === "none" || submenu.style.display === "") {
+        submenu.style.display = "block";
+        icon.classList.remove("bi-chevron-up"); 
+        icon.classList.add("bi-chevron-down");
+    } else {
         submenu.style.display = "none";
         icon.classList.remove("bi-chevron-down");
         icon.classList.add("bi-chevron-up");
-    } else {
-        submenu.style.display = "block";
-        icon.classList.remove("bi-chevron-up");
-        icon.classList.add("bi-chevron-down");
     }
 }
 
+function menu(event) {
+    // Engelleme için event.preventDefault() kullanılıyor
+    event.preventDefault();
+
+    var menu = document.getElementById("left");
+    var content = document.getElementById("right");
+    var space = document.getElementById("space");
+    var hb = document.getElementById("hb");
+
+    // Parse the width value as a float
+    var currentWidth = parseFloat(menu.style.width);
+
+    // Check if the menu is currently open (width is 20%)
+    if (currentWidth === 20) {
+        // If open, close the menu by setting widths accordingly
+        menu.style.display = "none";
+        menu.style.width = "0%";
+        space.style.width = "0%";
+        content.style.width = "100%";
+    } else {
+        // If closed, open the menu by setting widths accordingly
+        menu.style.display = "block";
+        menu.style.width = "20%";
+        space.style.width = "1%";
+        content.style.width = "79%";
+    }
+}
