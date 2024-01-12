1e405ef5e8aa4b775b164c7efacbda0dbf7ae995
EfeÖ <efeoztin07@gmail.com>
2024-01-12 23:36:46 +0300

    3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function subM(submenuId, iconId) {
     var icon = document.getElementById(iconId);
     if (submenu.style.display === "none" || submenu.style.display === "") {
         submenu.style.display = "block";
-        icon.classList.remove("bi-chevron-up"); 
+        icon.classList.remove("bi-chevron-up");
         icon.classList.add("bi-chevron-down");
     } else {
         submenu.style.display = "none";
@@ -19,23 +19,37 @@ function menu(event) {
     var menu = document.getElementById("left");
     var content = document.getElementById("right");
     var space = document.getElementById("space");
-    var hb = document.getElementById("hb");
-
+    var isMobile = window.matchMedia("(max-width: 800px)").matches;
     // Parse the width value as a float
     var currentWidth = parseFloat(menu.style.width);
-
-    // Check if the menu is currently open (width is 20%)
-    if (currentWidth === 20) {
-        // If open, close the menu by setting widths accordingly
-        menu.style.display = "none";
-        menu.style.width = "0%";
-        space.style.width = "0%";
-        content.style.width = "100%";
+    if (isMobile) { 
+        if (currentWidth === 100) {
+            // If open, close the menu by setting widths accordingly
+            menu.style.display = "none";
+            menu.style.width = "0%";
+            space.style.width = "0%";
+            content.style.width = "100%";
+        } else {
+            // If closed, open the menu by setting widths accordingly
+            menu.style.display = "block";
+            menu.style.width = "100%";
+            space.style.width = "0%";
+            content.style.width = "0%";
+        }
     } else {
-        // If closed, open the menu by setting widths accordingly
-        menu.style.display = "block";
-        menu.style.width = "20%";
-        space.style.width = "1%";
-        content.style.width = "79%";
+        // Check if the menu is currently open (width is 20%)
+        if (currentWidth === 20) {
+            // If open, close the menu by setting widths accordingly
+            menu.style.display = "none";
+            menu.style.width = "0%";
+            space.style.width = "0%";
+            content.style.width = "100%";
+        } else {
+            // If closed, open the menu by setting widths accordingly
+            menu.style.display = "block";
+            menu.style.width = "20%";
+            space.style.width = "1%";
+            content.style.width = "79%";
+        }
     }
 }
